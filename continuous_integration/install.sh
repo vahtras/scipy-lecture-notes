bed6ca033e73972634ec6dff22539561432a7f61
Olav Vahtras <vahtras@kth.se>
2016-09-02 14:21:57 +0000

    fix for local test

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+test -f continuous_integration/local && source continuous_integration/local
+test -f continuous_integration/functions && source continuous_integration/functions
+
 # This script is meant to be called by the "install" step defined in
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
 # The behavior of the script is controlled by environment variabled defined
@@ -15,74 +18,6 @@ set -e
 export CC=gcc
 export CXX=g++
 
-create_new_venv() {
-    # At the time of writing numpy 1.9.1 is included in the travis
-    # virtualenv but we want to be in control of the numpy version
-    # we are using for example through apt-get install
-    deactivate
-    virtualenv --system-site-packages testvenv
-    source testvenv/bin/activate
-    pip install nose
-}
-
-print_conda_requirements() {
-    # Echo a conda requirement string for example
-    # "pip nose python='2.7.3 scikit-learn=*". It has a hardcoded
-    # list of possible packages to install and looks at _VERSION
-    # environment variables to know whether to install a given package and
-    # if yes which version to install. For example:
-    #   - for numpy, NUMPY_VERSION is used
-    #   - for scikit-learn, SCIKIT_LEARN_VERSION is used
-    TO_INSTALL_ALWAYS="pip nose pillow"
-    REQUIREMENTS="$TO_INSTALL_ALWAYS"
-    TO_INSTALL_MAYBE="python numpy scipy matplotlib scikit-learn sympy scikit-image pandas seaborn statsmodels"
-    for PACKAGE in $TO_INSTALL_MAYBE; do
-        # Capitalize package name and add _VERSION
-        PACKAGE_VERSION_VARNAME="${PACKAGE^^}_VERSION"
-        # replace - by _, needed for scikit-learn for example
-        PACKAGE_VERSION_VARNAME="${PACKAGE_VERSION_VARNAME//-/_}"
-        # dereference $PACKAGE_VERSION_VARNAME to figure out the
-        # version to install
-        PACKAGE_VERSION="${!PACKAGE_VERSION_VARNAME}"
-        if [ -n "$PACKAGE_VERSION" ]; then
-            REQUIREMENTS="$REQUIREMENTS $PACKAGE=$PACKAGE_VERSION"
-        fi
-    done
-    echo $REQUIREMENTS
-}
-
-create_new_conda_env() {
-    # Skip Travis related code on circle ci.
-    if [ -z $CIRCLECI ]; then
-        # Deactivate the travis-provided virtual environment and setup a
-        # conda-based environment instead
-        deactivate
-    fi
-
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh \
-         -O miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda
-    export PATH=$HOME/miniconda/bin:$PATH
-    conda update --yes conda
-
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    REQUIREMENTS=$(print_conda_requirements)
-    echo "conda requirements string: $REQUIREMENTS"
-    conda create -n testenv --quiet --yes $REQUIREMENTS
-    source activate testenv
-    conda install --yes libgfortran=1
-
-    if [[ "$INSTALL_MKL" == "true" ]]; then
-        # Make sure that MKL is used
-        conda install --yes mkl
-    else
-        # Make sure that MKL is not used
-        conda remove --yes --features mkl || echo "MKL not installed"
-    fi
-}
 
 if [[ "$DISTRIB" == "neurodebian" ]]; then
     create_new_venv
@@ -94,7 +29,7 @@ elif [[ "$DISTRIB" == "conda" ]]; then
 
 else
     echo "Unrecognized distribution ($DISTRIB); cannot setup travis environment."
-    exit 1
+    #exit 1
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
